2421b321a6c39f44caf232899763856e2f04a1fb
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2015-12-05 09:46:25 +0000

    Finalized interfaces for PastMeeting and FutureMeeting

---
+++ FutureMeeting.java
@@ -0,0 +1,13 @@
+/**
+* A meeting to be held in the future
+*
+*
+*/
+public interface FutureMeeting extends Meeting {
+	/**
+	* No methods here, this is just naming interface.
+	* (i.e. only necessary for type checking and/or downcasting)
+	*
+	*/
+
+}
