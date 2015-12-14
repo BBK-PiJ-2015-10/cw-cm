84355cfe558182f39886fba41a2a7c64790d8404
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2015-12-14 17:40:30 +0000

    Uploading PastMeetingImpl .

---
+++ PastMeetingImpl.java
@@ -0,0 +1,47 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+	
+	/*
+	* This is to hold the meeting notes.
+	*/
+	private String notes;
+	
+	/*
+	* This is the Constructor for PastMeetingImpl.
+	* The notes must not be null or empty, otherwise IllegalArgumentException is thrown
+	*
+	*@throws NullPointerException if notes are null or empty.
+	*/
+	public PastMeetingImpl(int id, Calendar date, Set<Contact> participants, String notes){
+		super(id,date,participants);
+		testNotesNull(notes);
+		this.notes=notes;
+	}
+	
+	/*
+	*  Notes can't be null or empty. This method is to be leveraged in the Constructor method.
+	*/
+	public void testNotesNull (String notes){
+	    if ( (notes == null) || (notes.isEmpty()==true) ) {
+			throw new NullPointerException();
+		}
+	}
+	
+	/*
+	*  This is display the notes of the meeting.
+	*/
+	@Override
+	public String getNotes(){
+		return this.notes;
+	}
+	
+	/*
+	*  This is to manually set the notes of the meeting.
+	*/
+	public void setNotes(String notes){
+		this.notes=notes;
+	}
+
+}
