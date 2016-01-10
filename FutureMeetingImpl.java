7d475216cc62c9c4c3bda693b4b0a519b269c5ae
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2016-01-10 14:30:35 +0000

    Updated FutureMeetingImpl: Edited commentary to optimize readability .

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -1,9 +1,15 @@
 import java.util.Calendar;
 import java.util.Set;
+import java.io.Serializable;
 
-public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+/**
+* This is the implementation {@see FutureMeeting} with some additional methods.
+*
+* @author YAP
+*/
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable {
 		
-	/*
+	/**
 	* This is the Constructor for FutureMeetingImpl.
 	*/
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> participants){
